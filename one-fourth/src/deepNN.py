c119dcd596a414cbc6f1762a5b40fc3955bba47c
zhangting9756 <tingzhangguo@163.com>
2019-03-12 21:28:44 +0800

    one-four
    第一周第四次课

---
+++ one-fourth/src/deepNN.py
@@ -0,0 +1,222 @@
+# -*- coding: UTF-8 -*-
+import numpy as np
+import h5py
+import matplotlib.pyplot as plt
+import testCase
+from dnn_utils import sigmoid, sigmoid_backward, relu, relu_backward
+
+def initialize_parameters_deep(layers_dims):
+    """
+    此函数是为了初始化多层网络参数而使用的函数。
+    参数：
+        layers_dims - 包含我们网络中每个图层的节点数量的列表
+
+    返回：
+        parameters - 包含参数“W1”，“b1”，...，“WL”，“bL”的字典：
+                     W1 - 权重矩阵，维度为（layers_dims [1]，layers_dims [1-1]）
+                     bl - 偏向量，维度为（layers_dims [1]，1）
+    """
+    np.random.seed(3)
+    parameters = {}
+    L = len(layers_dims)
+
+    for l in range(1,L):
+        parameters["W" + str(l)] = np.random.randn(layers_dims[l], layers_dims[l - 1]) / np.sqrt(layers_dims[l - 1])
+        parameters["b" + str(l)] = np.zeros((layers_dims[l], 1))
+
+        #确保我要的数据的格式是正确的
+        assert(parameters["W" + str(l)].shape == (layers_dims[l], layers_dims[l-1]))
+        assert(parameters["b" + str(l)].shape == (layers_dims[l], 1))
+
+    return parameters
+
+def linear_forward(A,W,b):
+    """
+    实现前向传播的线性部分。
+
+    参数：
+        A - 来自上一层（或输入数据）的激活，维度为(上一层的节点数量，示例的数量）
+        W - 权重矩阵，numpy数组，维度为（当前图层的节点数量，前一图层的节点数量）
+        b - 偏向量，numpy向量，维度为（当前图层节点数量，1）
+
+    返回：
+         Z - 激活功能的输入，也称为预激活参数
+         cache - 一个包含“A”，“W”和“b”的字典，存储这些变量以有效地计算后向传递
+    """
+    Z = np.dot(W,A) + b
+    assert(Z.shape == (W.shape[0],A.shape[1]))
+    cache = (A,W,b)
+
+    return Z,cache
+
+def linear_activation_forward(A_prev,W,b,activation):
+    """
+    实现LINEAR-> ACTIVATION 这一层的前向传播
+
+    参数：
+        A_prev - 来自上一层（或输入层）的激活，维度为(上一层的节点数量，示例数）
+        W - 权重矩阵，numpy数组，维度为（当前层的节点数量，前一层的大小）
+        b - 偏向量，numpy阵列，维度为（当前层的节点数量，1）
+        activation - 选择在此层中使用的激活函数名，字符串类型，【"sigmoid" | "relu"】
+
+    返回：
+        A - 激活函数的输出，也称为激活后的值
+        cache - 一个包含“linear_cache”和“activation_cache”的字典，我们需要存储它以有效地计算后向传递
+    """
+
+    if activation == "sigmoid":
+        Z, linear_cache = linear_forward(A_prev, W, b)
+        A, activation_cache = sigmoid(Z)
+    elif activation == "relu":
+        Z, linear_cache = linear_forward(A_prev, W, b)
+        A, activation_cache = relu(Z)
+
+    assert(A.shape == (W.shape[0],A_prev.shape[1]))
+    cache = (linear_cache,activation_cache)
+
+    return A,cache
+
+def L_model_forward(X,parameters):
+    """
+    实现[LINEAR-> RELU] *（L-1） - > LINEAR-> SIGMOID计算前向传播，也就是多层网络的前向传播，为后面每一层都执行LINEAR和ACTIVATION
+
+    参数：
+        X - 数据，numpy数组，维度为（输入节点数量，示例数）
+        parameters - initialize_parameters_deep（）的输出
+
+    返回：
+        AL - 最后的激活值
+        caches - 包含以下内容的缓存列表：
+                 linear_relu_forward（）的每个cache（有L-1个，索引为从0到L-2）
+                 linear_sigmoid_forward（）的cache（只有一个，索引为L-1）
+    """
+    caches = []
+    A = X
+    L = len(parameters) // 2
+    for l in range(1,L):
+        A_prev = A 
+        A, cache = linear_activation_forward(A_prev, parameters['W' + str(l)], parameters['b' + str(l)], "relu")
+        caches.append(cache)
+
+    AL, cache = linear_activation_forward(A, parameters['W' + str(L)], parameters['b' + str(L)], "sigmoid")
+    caches.append(cache)
+
+    assert(AL.shape == (1,X.shape[1]))
+
+    return AL,caches
+
+def compute_cost(AL,Y):
+    """
+    实施等式（4）定义的成本函数。
+
+    参数：
+        AL - 与标签预测相对应的概率向量，维度为（1，示例数量）
+        Y - 标签向量（例如：如果不是猫，则为0，如果是猫则为1），维度为（1，数量）
+
+    返回：
+        cost - 交叉熵成本
+    """
+    m = Y.shape[1]
+    cost = -np.sum(np.multiply(np.log(AL),Y) + np.multiply(np.log(1 - AL), 1 - Y)) / m
+
+    cost = np.squeeze(cost)
+    assert(cost.shape == ())
+
+    return cost
+
+def linear_backward(dZ,cache):
+    """
+    为单层实现反向传播的线性部分（第L层）
+
+    参数：
+         dZ - 相对于（当前第l层的）线性输出的成本梯度
+         cache - 来自当前层前向传播的值的元组（A_prev，W，b）
+
+    返回：
+         dA_prev - 相对于激活（前一层l-1）的成本梯度，与A_prev维度相同
+         dW - 相对于W（当前层l）的成本梯度，与W的维度相同
+         db - 相对于b（当前层l）的成本梯度，与b维度相同
+    """
+    A_prev, W, b = cache
+    m = A_prev.shape[1]
+    dW = np.dot(dZ, A_prev.T) / m
+    db = np.sum(dZ, axis=1, keepdims=True) / m
+    dA_prev = np.dot(W.T, dZ)
+
+    assert (dA_prev.shape == A_prev.shape)
+    assert (dW.shape == W.shape)
+    assert (db.shape == b.shape)
+
+    return dA_prev, dW, db
+
+def linear_activation_backward(dA,cache,activation="relu"):
+    """
+    实现LINEAR-> ACTIVATION层的后向传播。
+
+    参数：
+         dA - 当前层l的激活后的梯度值
+         cache - 我们存储的用于有效计算反向传播的值的元组（值为linear_cache，activation_cache）
+         activation - 要在此层中使用的激活函数名，字符串类型，【"sigmoid" | "relu"】
+    返回：
+         dA_prev - 相对于激活（前一层l-1）的成本梯度值，与A_prev维度相同
+         dW - 相对于W（当前层l）的成本梯度值，与W的维度相同
+         db - 相对于b（当前层l）的成本梯度值，与b的维度相同
+    """
+    linear_cache, activation_cache = cache
+    if activation == "relu":
+        dZ = relu_backward(dA, activation_cache)
+        dA_prev, dW, db = linear_backward(dZ, linear_cache)
+    elif activation == "sigmoid":
+        dZ = sigmoid_backward(dA, activation_cache)
+        dA_prev, dW, db = linear_backward(dZ, linear_cache)
+
+    return dA_prev,dW,db
+
+if __name__ == '__main__':
+    print("==============测试initialize_parameters_deep==============")
+    layers_dims = [5,4,3]
+    parameters = initialize_parameters_deep(layers_dims)
+    print("W1 = " + str(parameters["W1"]))
+    print("b1 = " + str(parameters["b1"]))
+    print("W2 = " + str(parameters["W2"]))
+    print("b2 = " + str(parameters["b2"]))
+    
+    #测试linear_forward
+    print("==============测试linear_forward==============")
+    A,W,b = testCase.linear_forward_test_case()
+    Z,linear_cache = linear_forward(A,W,b)
+    print("Z = " + str(Z))
+    
+    #测试linear_activation_forward
+    print("==============测试linear_activation_forward==============")
+    A_prev, W,b = testCase.linear_activation_forward_test_case()
+    
+    A, linear_activation_cache = linear_activation_forward(A_prev, W, b, activation = "sigmoid")
+    print("sigmoid，A = " + str(A))
+    
+    A, linear_activation_cache = linear_activation_forward(A_prev, W, b, activation = "relu")
+    print("ReLU，A = " + str(A))
+
+    #测试L_model_forward
+    print("==============测试L_model_forward==============")
+    X,parameters = testCase.L_model_forward_test_case()
+    AL,caches = L_model_forward(X,parameters)
+    print("AL = " + str(AL))
+    print("caches 的长度为 = " + str(len(caches)))
+
+    #测试compute_cost
+    print("==============测试compute_cost==============")
+    Y,AL = testCase.compute_cost_test_case()
+    print("cost = " + str(compute_cost(AL, Y)))
+    
+    #测试linear_backward
+    print("==============测试linear_backward==============")
+    dZ, linear_cache = testCase.linear_backward_test_case()
+    
+    dA_prev, dW, db = linear_backward(dZ, linear_cache)
+    print ("dA_prev = "+ str(dA_prev))
+    print ("dW = " + str(dW))
+    print ("db = " + str(db))
+
+
+
